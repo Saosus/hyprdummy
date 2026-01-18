7b77612edbd0fa5978c4a93a8091a328fe1c23da
CPPYLI <m4rsel.nizamov@gmail.com>
2026-01-18 21:44:57 +0500

    created hyprland dotfiles

---
+++ hypr/scripts/wallpaper-changer.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+function wp_is_new () {
+	local new_wallpaper="$1"
+
+	if [ -z "$new_wallpaper" ];
+	then
+		exit 1
+	fi
+
+	local CURRENT_WALLPAPER=$(swww query | grep -oP 'image: \K.*' | head -1)
+
+	if [ "$1" = "$CURRENT_WALLPAPER" ]
+	then
+		echo "regenerate"
+	else
+		echo "$1"
+	fi
+}		
+
+# Директория с обоями
+WALLPAPER_DIR="/home/cppyli/Media/Wallpapers/Solyanka"
+
+# Проверяем, существует ли директория
+if [ ! -d "$WALLPAPER_DIR" ]; then
+    notify-send "Ошибка: директория $WALLPAPER_DIR не найдена."
+    exit 1
+fi
+
+# Ищем случайное изображение
+WALLPAPER=""
+FLAG="regenerate"
+
+while [ "$FLAG" = "regenerate" ]
+do
+	WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) | shuf -n 1)
+	
+	# Проверяем, найден ли файл
+	if [ -z "$WALLPAPER" ]; then
+    		notify-send "Ошибка: в директории нет изображений (jpg, png, jpeg)."
+    		exit 1
+	fi
+	
+	#Проверка, что обои не текущие
+	if [ $(wp_is_new $WALLPAPER) != "regenerate" ]
+	then
+		FLAG=""
+	else
+		continue
+	fi
+done
+
+# Меняем обои 
+swww img "$WALLPAPER" --transition-bezier 0.5,1,0.89,1 --transition-type wipe --transition-fps 60 --transition-duration 1.2 
+
+#Звуковой эффект
+mpv ~/Media/Sounds/haki1.mp3 --volume=30 --no-video --no-terminal & 
+
+sleep 0.4 # задержка для синхронности с walrs
+
+# Меняем цветовую схему через walrs
+walrs -q -i "$WALLPAPER" -b 20 -s 150 -W 
+walogram # смена темы telegram 
+
+# перезапускаем службу уведомлений
+makoctl reload 
+
