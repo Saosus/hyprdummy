783eca10d4d41c1b543b90502c2808decc7974d0
cppyli <m4rsel.nizamov@gmail.com>
2026-09-14 12:55:43 +0500

    fixed pull issues

--- .config/eww/scripts/spotifyctl.sh
+++ .config/eww/scripts/spotifyctl.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
-
-<<<<<<< HEAD
-eww=/home/cppyli/Documents/githubClones/eww/target/release/eww
-
-if [[ -z $($eww active-windows | grep 'music-box') ]]; then
-    $eww open music-box && $eww update music-revealer=true
-else
-    $eww update music-revealer=false
-    (sleep 0.2 && $eww close music-box) &
-=======
-
 if [[ -z $(eww active-windows | grep 'music-box') ]]; then
     eww open music-box && eww update music-revealer=true
 else
     eww update music-revealer=false
     (sleep 0.2 && eww close music-box) &
->>>>>>> 9d211cf (reinitialized repo)
 fi
